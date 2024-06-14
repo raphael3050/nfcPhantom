9d72ea667c664d3cef09c171243617e9e4598d91
Raphaël AZOU <raphael.azou@ensta-bretagne.org>
2024-06-14 17:43:35 +0200

    [style] - 🎨 Boot image first implementation

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,18 @@
 #include <OneButton.h>
 #include <TFT_eSPI.h>
 #include <PN532.h>
-
 #include <NfcAdapter.h>
 #include <Adafruit_PN532.h>
-
+#include <boot_img.h>
 #define PIN_POWER_ON 15
 #define PIN_BUTTON_1 0
 #define PIN_BUTTON_2 14
 
 int sda = 18;
 int scl = 17;
-int message_displayed = 0;
 int start_read = 0;
 volatile bool connected = false;
 
-OneButton button(PIN_BUTTON_1, true);
 OneButton button_start_read(PIN_BUTTON_2, true);
 TFT_eSPI tft = TFT_eSPI();
 Adafruit_PN532 nfc(sda, scl);
@@ -38,57 +35,6 @@ void displayMessage(char const *message, boolean newline = true)
 }
 
 
-void deviceScan(TwoWire *_port, Stream *stream)
-{
-    uint8_t err, addr;
-    int nDevices = 0;
-    for (addr = 1; addr < 127; addr++)
-    {
-        _port->beginTransmission(addr);
-        err = _port->endTransmission();
-        if (err == 0)
-        {
-            stream->print("[+] I2C device found at address 0x");
-            tft.print("[+] I2C device found at address 0x");
-            if (addr < 16)
-            {
-                stream->print("0");
-                tft.print("0");
-            }
-            stream->print(addr, HEX);
-            stream->println(" !");
-            tft.print(addr, HEX);
-            tft.println(" !");
-            nDevices++;
-        }
-        else if (err == 4)
-        {
-            stream->print("[+] Unknow error at address 0x");
-            if (addr < 16)
-                stream->print("0");
-            stream->println(addr, HEX);
-        }
-    }
-    if (nDevices == 0)
-    {
-        stream->println("[+] No I2C devices found\n");
-        tft.println("[+] No I2C devices found\n");
-    }
-    else
-    {
-        stream->println("[+] Done\n");
-        tft.setTextColor(TFT_GREEN, TFT_BLACK);
-        tft.println("[+] Done\n");
-        tft.setTextColor(TFT_WHITE, TFT_BLACK);
-    }
-}
-
-void simpleClick()
-{
-    displayMessage("[+] Scanning I2C devices...");
-    deviceScan(&Wire, &Serial);
-}
-
 void startRecordClick()
 {
     displayMessage("[+] Scanning NFC devices...");
@@ -102,14 +48,17 @@ void setup()
     Serial.begin(115200);
     //Wire.begin(sda, scl);
 
-    button.attachClick(simpleClick);
     button_start_read.attachClick(startRecordClick);
     tft.init();
     tft.setRotation(3);
     tft.fillScreen(TFT_BLACK);
+    tft.pushImage(0, 0, 320, 88, phantom_icon);
     tft.setTextColor(TFT_WHITE, TFT_BLACK);
     tft.setTextSize(1);
     tft.setCursor(0, 0);
+
+    nfc.begin();
+    delay(100);
 
     nfc.begin();
     nfc.SAMConfig();
@@ -118,11 +67,6 @@ void setup()
 
 void loop()
 {
-    if (message_displayed == 0)
-    {
-        displayMessage("[+] Press the button to start scanning I2C devices...");
-        message_displayed = 1;
-    }
     if (start_read == 1)
     {
         uint8_t success;
@@ -131,13 +75,12 @@ void loop()
         char buffer[32]; // for the itoa function
 
         // Essayer de lire l'UID de la carte NFC
+        nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
         success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
         if (success)
         {
             displayMessage("[+] Found an NFC card!");
-            // Afficher l'UID de la carte NFC
             displayMessage("[+] UID Length: ", false);
-
             displayMessage(itoa(uidLength, buffer, 10), false);            
             displayMessage(" bytes");
             Serial.print("[+] UID Value: ");
@@ -145,14 +88,13 @@ void loop()
             for (uint8_t i = 0; i < uidLength; i++)
             {
                 displayMessage(" 0x", (boolean)false);
-                Serial.print(uid[i], HEX);
+                displayMessage(itoa(uid[i], buffer, 16), false);
             }
             Serial.println("");
 
             delay(1000);
         }
     }
-    button.tick();
     button_start_read.tick();
     delay(100);
 }
